7fd6127ab2416cbf067cc6bc6ef8f7b61928c0c7
BugHunter-X <askzenai@gmail.com>
2025-02-07 22:56:05 +0100

    Add bugSolution.cob file

---
+++ bugSolution.cob
@@ -0,0 +1,14 @@
+01  WS-AREA-1.          PIC X(100). 
+01  WS-AREA-2.          PIC 9(5) VALUE 0. 
+01  WS-NUMERIC-AREA.   PIC 9(5).
+
+PROCEDURE DIVISION.
+    MOVE 'some data' TO WS-AREA-1
+    IF WS-AREA-1 NUMERIC THEN
+        MOVE WS-AREA-1 TO WS-NUMERIC-AREA
+        ADD WS-NUMERIC-AREA TO WS-AREA-2
+    ELSE
+        DISPLAY "Error: Non-numeric data encountered." 
+    END-IF
+    DISPLAY WS-AREA-1 WS-AREA-2
+    STOP RUN.
